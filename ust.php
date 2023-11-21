ffc1a541cc6a764bef05f2967310a88203b11031
Ertekinu <ertekinufuk@gmail.com>
2023-11-22 02:03:17 +0300

    dadsadasdsad

--- ust.php
+++ ust.php
@@ -13,7 +13,8 @@ $sites  = $vt->query("select * from siteayar")->fetchAll(PDO::FETCH_ASSOC); fore
 
 <head>
 	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="noindex">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title><?=$sites["site_baslik"];?></title>
 	<link rel="preconnect" href="https://fonts.googleapis.com/">
 	<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
